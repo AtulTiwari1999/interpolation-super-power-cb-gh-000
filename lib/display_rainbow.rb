6936a98c9d25a6a0ef994f9790681b86b5560b01
Atul Tiwari <atultiwarix7@gmail.com>
2019-07-04 14:34:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts
+  puts"R:#{colors[0]}, O:#{colors[1]},O:#{colors[2]},O:#{colors[3]},O:#{colors[4]},O:#{colors[5]},O:#{colors[6]} "
